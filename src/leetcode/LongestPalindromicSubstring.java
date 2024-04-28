bc16fe521f9b99aaaefcca1e4e71000a2af1f7b3
Gnanaprakash22 <gnanaprakash704@gmail.com>
2024-04-29 02:02:36 +0530

    added longest palindromic substring

---
+++ src/leetcode/LongestPalindromicSubstring.java
@@ -0,0 +1,54 @@
+package leetcode;
+
+public class LongestPalindromicSubstring {
+    public static void main(String[] args) {
+        String str="cbbd";
+        System.out.println(longestPalindrome(str));
+    }
+
+    private static String longestPalindrome(String s){
+        if(s.length()<=1)
+            return s;
+
+        String LPS="";
+
+        //babad
+        for(int i=1;i<s.length();i++){
+            int high=i;
+            int low=i;
+
+            while (s.charAt(low)==s.charAt(high)){
+                low--;
+                high++;
+
+                if(low==-1 || high==s.length())
+                    break;
+            }
+
+            String palindrome=s.substring(low+1,high);
+            if(LPS.length()<palindrome.length()){
+                LPS=palindrome;
+            }
+        }
+
+        for(int i=1;i<s.length();i++){
+            int high=i;
+            int low=i-1;
+
+            while (s.charAt(low)==s.charAt(high)){
+                low--;
+                high++;
+
+                if(low==-1 || high==s.length())
+                    break;
+            }
+
+            String palindrome=s.substring(low+1,high);
+            if(LPS.length()<palindrome.length()){
+                LPS=palindrome;
+            }
+        }
+
+        return LPS;
+    }
+}
